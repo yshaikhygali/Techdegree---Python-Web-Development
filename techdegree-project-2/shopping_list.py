6a83e9da494aa8965c2be5830411d8b75d7b5073
yshaikhygali <43465744+yshaikhygali@users.noreply.github.com>
2019-03-17 12:58:18 +0600

    Create shopping_list.py

---
+++ techdegree-project-2/shopping_list.py
@@ -0,0 +1,73 @@
+import os 
+
+shopping_list = []
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+# os library lets me look at information about the system that my code 
+# is running on
+
+def show_help():
+    clear_screen()
+    print("What should we pick up at the store?")
+    print("""
+Enter 'DONE' to stop adding items.
+Enter 'HELP' for this help.
+Enter 'SHOW' to see your current list.
+""")
+    
+    
+def add_to_list(item):
+    show_list()
+    if len(shopping_list):
+        position = input("Where should I add {}?\n"
+                         "Press ENTER to add to the end of the list\n"
+                         "> ".format(item))
+    else:
+        position = 0
+     
+    try:
+        position = abs(int(position))
+    except ValueError:
+        position = None
+    if position is not None:
+        shopping_list.insert(position-1, item)
+    else:
+        shopping_list.append(new_item)
+        
+    show_list()
+    
+    
+
+def show_list():
+    clear_screen()
+    
+    print("Here's your list:")
+    
+    index =1
+    for item in shopping_list:
+        print("{}. {}".format(index,item))
+        index +=1
+        
+    print("-"*10)
+        
+show_help()
+
+while True:
+    new_item = input("> ")
+    
+    if new_item.upper() == "DONE" or new_item.upper() == "QUIT":
+        break
+    elif new_item.upper() == "HELP":
+        show_help()
+        continue
+    elif new_item.upper() == "SHOW":
+        show_list()
+        continue
+    else:
+        add_to_list(new_item)
+
+show_list()
+        
+    
+    
